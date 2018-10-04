336b837d69fa49de793ff3c12d33ef02d3bea7d2
Duane Felton <dfelton2010@gmail.com>
2018-10-04 14:37:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,19 +4,7 @@
 
 
 def key_for_min_value(name_hash)
-  name_hash = name_hash.to_a
-  
-  a_val = 1000
-  a_key = 0
-  
-  name_hash.each do |i|
-    if i[1] < a_value
-      a_value = i[1]
-      a_key = i[0]
-    end
-end
-
-return a_key
+  name_hash = name
 
 end
 
